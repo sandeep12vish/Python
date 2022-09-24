52ae769aac2a9dbcdb327502e50b34210e27535c
Sandeep <sandeep12vish@gmail.com>
2022-09-25 00:06:58 +0530

    accept 2 number and swap them

---
+++ day2_swap_to_number.py
@@ -0,0 +1,13 @@
+# Program to swap the number
+print("Please enter the values of a & b\n")
+num1=input("a: ") 
+num2=input("b: ")
+print("\nBefore swap\n"+"a= "+num1)
+print("b= "+num2)
+## logic to swap two number
+temp=num1
+num1=num2
+num2=temp
+#After swap
+print("\nAfter swap\n"+"a= "+num1)
+print("b= "+num2)
